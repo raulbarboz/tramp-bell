f3eca9481886c3cb5b22bf885617cf2095b8eeb4
raulbarboz <ruzito@gmail.com>
2019-04-18 18:05:34 -0300

    dotenv added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+require('dotenv').config({path:'.env'});
 
 module.exports = {
     entry: './src/app.js',
@@ -20,6 +21,14 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html'
+        }),
+        new HtmlWebpackPlugin.DefinePlugin({
+            'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+            'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+            'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+            'process.env.PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
+            'process.env.STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET),
+            'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID),
         })
     ]
 };
