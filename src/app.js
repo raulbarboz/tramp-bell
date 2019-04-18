46d32b4a1c44020e0700b4cf4e4ec748ae03e0e9
Raul Barboza <ruzito@gmail.com>
2019-04-17 21:26:42 -0300

    added bootstrap

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     render(){
         return(
-            <div>Webpack Setup Ok</div>
+            <div className="container">
+                <div className="jumbotron">
+                    <h1>Hello, world!</h1>
+                    <p>...</p>
+                </div>
+            </div> 
         )
     }
 }
